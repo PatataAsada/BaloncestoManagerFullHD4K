95bb408a39dad505e878b17cb17ccaa2f8b8c448
Yeray <yeray3000@gmail.com>
2019-02-12 11:10:23 +0100

    modificación -> preparado post para llamar a función cambiar_liga() con datos, falta operación medoo

--- funciones/subir_liga.php
+++ funciones/subir_liga.php
@@ -1,5 +1,15 @@
 <?php
-//TODO script para añadir la primera liga o actualizar la existente y volver al menu
+require './funciones.php';
 
+if($_POST){
+    $nom_liga = $_POST['nom_liga'];
+    $anno_liga_inicio = $_POST['anno_liga_inicio'];
+    $anno_liga_fin = $_POST['anno_liga_fin'];
+    $liga_desc = $_POST['liga_desc'];
+
+    //TODO funcion medoo con esto.
+    cambiar_liga($nom_liga,$anno_liga_inicio,$anno_liga_fin,$liga_desc);
+    
+}
 header('Location:../inicio.php');
 ?>
