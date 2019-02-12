47fad09edc9b30811582d53ac268e098058e673b
Yeray <yeray3000@gmail.com>
2019-02-12 13:10:52 +0100

    modificacion -> subir_liga.php  cambio de funcion cambiar_liga() a una que puede ser mas genérica

--- funciones/subir_liga.php
+++ funciones/subir_liga.php
@@ -7,8 +7,9 @@ if($_POST){
     $anno_liga_fin = $_POST['anno_liga_fin'];
     $liga_desc = $_POST['liga_desc'];
 
+    $tabla = "liga";
     //TODO funcion medoo con esto.
-    cambiar_liga($nom_liga,$anno_liga_inicio,$anno_liga_fin,$liga_desc);
+    update_table($tabla,$nom_liga,$anno_liga_inicio,$anno_liga_fin,$liga_desc);
     
 }
 header('Location:../inicio.php');
