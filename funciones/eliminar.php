72e4e8b4e40b3f0bafab039d3f9f9c8927b003da
Rickdam <Grievous1234>
2019-03-03 20:03:16 +0100

    funciones saneá, eliminarphp y scriptsql

--- funciones/eliminar.php
+++ funciones/eliminar.php
@@ -1,3 +1,8 @@
 <?php
-//TODO eliminar el resultado o equipo que venga por $_POST.
-?>
+include "funciones.php";
+
+if (isset($_POST['opcion'])) {
+    if (isset($_POST['tabla']) && isset($_POST['pk'])) {
+        delete_data($_POST['pk'], $_POST['tabla']);
+    }
+}
