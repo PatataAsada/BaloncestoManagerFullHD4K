ed5f94c609c5cd3979c11ffeb2c645b19f75f8a4
Yeray <yeray3000@gmail.com>
2019-03-05 09:27:04 +0100

    base de datos con usuario y contraseña

--- logica/nba_proyecto.sql
+++ logica/nba_proyecto.sql
@@ -1,45 +1,126 @@
--- noinspection SqlNoDataSourceInspectionForFile
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 05-03-2019 a las 09:22:41
+-- Versión del servidor: 10.1.35-MariaDB
+-- Versión de PHP: 7.2.9
 
-DROP DATABASE IF EXISTS baloncesto;
-CREATE DATABASE baloncesto;
-USE baloncesto;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-CREATE TABLE liga (
-  nombre varchar(40) NOT NULL,
-  anio_inicio Date NOT NULL,
-  anio_fin Date NOT NULL,
-  descripcion varchar(200) DEFAULT NULL,
-  PRIMARY KEY (nombre)
-)engine=innodb;
 
-CREATE TABLE equipos (
-  nombre varchar(20) NOT NULL,
-  ciudad varchar(20) DEFAULT NULL,
-  num_socios int NOT NULL,
-  anio TIMESTAMP NOT NULL,
-  PRIMARY KEY (nombre)
-)engine=innodb;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
--- CREATE TABLE jugadores (
---   codigo int NOT NULL,
---   nombre varchar(30) DEFAULT NULL,
---   procedencia varchar(20) DEFAULT NULL,
---   altura varchar(4) DEFAULT NULL,
---   peso int DEFAULT NULL,
---   posicion varchar(5) DEFAULT NULL,
---   nombre_equipo varchar(20) DEFAULT NULL,
---   PRIMARY KEY (codigo),
---   FOREIGN KEY (nombre_equipo) References equipos(nombre)
--- )engine=innodb;
+--
+-- Base de datos: `baloncesto`
+--
+DROP DATABASE IF EXISTS `baloncesto`;
+CREATE DATABASE IF NOT EXISTS `baloncesto` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `baloncesto`;
 
-CREATE TABLE partidos (
-  codigo int NOT NULL,
-  equipo_local varchar(20) DEFAULT NULL,
-  equipo_visitante varchar(20) DEFAULT NULL,
-  puntos_local int DEFAULT NULL,
-  puntos_visitante int DEFAULT NULL,
-  temporada varchar(5) DEFAULT NULL,
-  PRIMARY KEY (codigo),
-  FOREIGN KEY (equipo_local) REFERENCES equipos(nombre) ON DELETE CASCADE,
-  FOREIGN KEY (equipo_visitante) REFERENCES equipos(nombre) ON DELETE CASCADE
-)engine=innodb;
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `equipos`
+--
+
+CREATE TABLE `equipos` (
+  `nombre` varchar(20) NOT NULL,
+  `ciudad` varchar(20) DEFAULT NULL,
+  `num_socios` int(11) NOT NULL,
+  `anio` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `equipos`
+--
+
+INSERT INTO `equipos` (`nombre`, `ciudad`, `num_socios`, `anio`) VALUES
+('equipo2', 'ciudad2', 100, '2019-03-05 08:13:15'),
+('equipo3', 'ciudad3', 40, '2019-03-05 08:16:19');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `liga`
+--
+
+CREATE TABLE `liga` (
+  `nombre` varchar(40) NOT NULL,
+  `anio_inicio` date NOT NULL,
+  `anio_fin` date NOT NULL,
+  `descripcion` varchar(200) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `liga`
+--
+
+INSERT INTO `liga` (`nombre`, `anio_inicio`, `anio_fin`, `descripcion`) VALUES
+('liga1', '2019-03-21', '2019-03-15', 'una liga');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `partidos`
+--
+
+CREATE TABLE `partidos` (
+  `codigo` int(11) NOT NULL,
+  `equipo_local` varchar(20) DEFAULT NULL,
+  `equipo_visitante` varchar(20) DEFAULT NULL,
+  `puntos_local` int(11) DEFAULT NULL,
+  `puntos_visitante` int(11) DEFAULT NULL,
+  `temporada` varchar(5) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `equipos`
+--
+ALTER TABLE `equipos`
+  ADD PRIMARY KEY (`nombre`);
+
+--
+-- Indices de la tabla `liga`
+--
+ALTER TABLE `liga`
+  ADD PRIMARY KEY (`nombre`);
+
+--
+-- Indices de la tabla `partidos`
+--
+ALTER TABLE `partidos`
+  ADD PRIMARY KEY (`codigo`),
+  ADD KEY `equipo_local` (`equipo_local`),
+  ADD KEY `equipo_visitante` (`equipo_visitante`);
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `partidos`
+--
+ALTER TABLE `partidos`
+  ADD CONSTRAINT `partidos_ibfk_1` FOREIGN KEY (`equipo_local`) REFERENCES `equipos` (`nombre`) ON DELETE CASCADE,
+  ADD CONSTRAINT `partidos_ibfk_2` FOREIGN KEY (`equipo_visitante`) REFERENCES `equipos` (`nombre`) ON DELETE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+GRANT SELECT, INSERT, UPDATE, DELETE, FILE ON *.* TO 'pipo'@'localhost' IDENTIFIED BY PASSWORD '*9A2A271CFB461EFBA06F712B856B14E3C8E8BCF2';
+
+GRANT SELECT, INSERT, UPDATE, DELETE ON `baloncesto`.* TO 'pipo'@'localhost';
